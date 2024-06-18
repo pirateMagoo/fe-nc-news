c715236bc90ee478e88956689162a18374bca8b1
pirateMagoo <williammckenzie275@gmail.com>
2024-06-18 09:26:25 +0100

    provides single page view functionality for ticket 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import './App.css'
-import HomePage from './Components/HomePage';
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import ArticlePage from "./Components/ArticlePage";
 
 function App() {
-  
-
   return (
-  <main>
-    <HomePage />
-  </main>
-  )
+    
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/articles/:article_id" element={<ArticlePage />} />
+        </Routes>
+      </main>
+    
+  );
 }
 
-export default App
+export default App;
